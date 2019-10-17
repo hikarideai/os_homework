c7a35e4b53ae23799f316b98cf3e9dbd76d46b25
hikarideai <16307792+hikarideai@users.noreply.github.com>
2019-10-17 22:36:33 +0300

    Week 9 Assignment v2

--- week9/ex1.c
+++ week9/ex1.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
+#include <limits.h>
 
 // How many page frames are available to the user
 #define PFRM_COUNT 100
 
 // How many page references occur in a single clock tick
-#define CLOCK_INT 100
+#define CLOCK_INT 1
 
-struct page {
+// How much bits are used to store age
+#define MEM_FRAME 64
+
+typedef struct {
     int i;
-    int age;
-};
-typedef struct page page_t;
+    uint64_t age;
+} page_t;
 
 int main() {
     page_t pmap[PFRM_COUNT];
-    char rbit[PFRM_COUNT];
+    unsigned char rbit[PFRM_COUNT];
     memset(pmap, 0, sizeof(pmap));
     memset(rbit, 0, sizeof(rbit));
 
-    FILE* file = fopen("Lab 09 input.txt", "r");
+    FILE* file = fopen("input.xt", "r");
     if (file == NULL) {
         printf("Could't open the input file\n");
         return 1;
@@ -30,9 +35,14 @@ int main() {
 
     while (!feof(file)) {
         fscanf(file, "%d", &page_req);
+        if (feof(file))
+            break;
+
         if (page_cnt % CLOCK_INT == 0) {
-            for (int i = 0; i < PFRM_COUNT; ++i)
-                pmap[i].age = ((rbit[i] & 1) << 31) | (pmap[i].age >> 1);
+            for (int i = 0; i < PFRM_COUNT; ++i) {
+                pmap[i].age = ((uint64_t)(rbit[i] & 1) << (MEM_FRAME - 1)) | (pmap[i].age >> 1);
+                rbit[i] = 0;
+            }
         }
         page_cnt++;
 
@@ -40,18 +50,20 @@ int main() {
         for (int i = 0; i < PFRM_COUNT; ++i) {
             if (pmap[i].i == page_req)
                 x = i;
-            if (pmap[i].age > pmap[oldest].age)
+            if (pmap[i].age < pmap[oldest].age)
                 oldest = i;
         }
 
         if (x != -1) {
             hits++;
+            rbit[x] = 1;
             continue;
         }
 
         misses++;
         pmap[oldest].i = page_req;
-        pmap[oldest].age = (1 << 31);
+        pmap[oldest].age = 0;
+        rbit[oldest] = 1;
     }
 
     printf("Hits\tMisses\n%d\t%d\n", hits, misses);
